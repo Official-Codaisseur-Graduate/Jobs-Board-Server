529a6a591c690624e67e334728aec31cf95e1c80
JetskevdWouden <zegmaarjet@gmail.com>
2019-07-24 11:49:35 +0200

    added a members model & router - POST request to GET all members

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const cors = require('cors')
 const huntrRouter = require('./Huntr/companies/routes')
 const indeedRouter = require('./Indeed/routes')
 const jobsRouter = require('./Huntr/jobs/router')
+const membersRouter = require('./Huntr/members/router')
 
 const app = express()
 const port = process.env.PORT || 4000
 app
   .use(cors())
   .use(bodyParser.json())
-  .use(huntrRouter, indeedRouter, jobsRouter)
+  .use(huntrRouter, indeedRouter, jobsRouter, membersRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
 
+  
